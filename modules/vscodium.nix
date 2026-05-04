2dfa4909cf6c635a4907b020358b53a19afd2585
tomwrw <tomwrw@proton.me>
2026-05-04 07:46:39 +0100

    fix: Updated programs.vscode to programs.vscodium.

--- modules/vscodium.nix
+++ modules/vscodium.nix
@@ -2,9 +2,8 @@
   flake.modules.homeManager.pc =
     { pkgs, ... }:
     {
-      programs.vscode = {
+      programs.vscodium = {
         enable = true;
-        package = pkgs.vscodium;
         profiles = {
           default = {
             extensions = with pkgs.vscode-extensions; [
