e713b4f6997412cba3488abfe4e20affc812acf5
tomwrw <tomwrw@proton.me>
2026-08-03 13:28:23 +0100

    fix: Drop redundant lib on schema.nix.

--- modules/schema.nix
+++ modules/schema.nix
@@ -1,19 +1,17 @@
 { lib, ... }:
 {
-  den.schema.host =
-    { lib, ... }:
-    {
-      options.syncthing = {
-        enable = lib.mkEnableOption "participation in the Syncthing mesh" // {
-          default = true;
-        };
-        id = lib.mkOption {
-          type = lib.types.str;
-          default = "";
-          description = "This host's Syncthing device ID (must match its provisioned cert).";
-        };
+  den.schema.host = {
+    options.syncthing = {
+      enable = lib.mkEnableOption "participation in the Syncthing mesh" // {
+        default = true;
+      };
+      id = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        description = "This host's Syncthing device ID (must match its provisioned cert).";
       };
     };
+  };
 
   den.schema.user = {
     config.classes = lib.mkDefault [ "homeManager" ];
